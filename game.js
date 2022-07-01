5b9302238a01114fe7865359f27ce15e5251bd4b
John Colagioia <john@colagioia.net>
2022-07-01 06:35:40 -0400

    Add stub JavaScript file

---
+++ game.js
@@ -0,0 +1,20 @@
+questions = {};
+
+window.addEventListener('load', (e) => {
+  getQuestions(8);
+});
+
+function getQuestions (n) {
+  const request = new XMLHttpRequest();
+  
+  request.open('GET', `https://opentdb.com/api.php?amount=${n}`, false);
+  request.send(null);
+
+  if (request.status !== 200) {
+    alert('Problem with networking.');
+    return;
+  }
+  
+  questions = JSON.parse(request.responseText);
+}
+
