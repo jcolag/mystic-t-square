268431dd0d8287f8ccba59f8abee236838f060a2
John Colagioia <john@colagioia.net>
2022-07-06 06:52:39 -0400

    Show new question on clicking a table cell

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ let questions = {};
 let answering = null;
 
 window.addEventListener('load', (e) => {
+  Array.from(document.getElementsByTagName('td'))
+    .forEach((cell) => cell.addEventListener('click', handleCellClick));
   getQuestions(8);
 });
 
@@ -22,6 +24,34 @@ function getQuestions (n) {
   questions = JSON.parse(request.responseText);
 }
 
+function handleCellClick(event) {
+  const trivia = document.getElementById('right-panel');
+  const panel = document.createElement('div');
+  const q = questions.results[questionIndex];
+  const question = document.createTextNode(unescape(q.question));
+  const line = document.createElement('br');
+  const answers = [];
+
+  answers.push(q.correct_answer);
+  q.incorrect_answers.forEach((a) => answers.push(a));
+  panel.id = questionIndex;
+  panel.appendChild(question);
+  panel.appendChild(line);
+  answers
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a,b) => a.sort - b.sort)
+    .map(({value}) => value)
+    .forEach((a) => {
+      const answer = document.createElement('button');
+      const answerText = document.createTextNode(unescape(a));
+
+      answer.addEventListener('click', handleAnswerClick);
+      answer.appendChild(answerText);
+      panel.appendChild(answer);
+    });
+  trivia.appendChild(panel);
+}
+
 function unescape(input) {
   const doc = new DOMParser()
     .parseFromString(input, "text/html");
@@ -29,7 +59,6 @@ function unescape(input) {
   return doc.documentElement.textContent;
 }
 
-function checkWin(onWin) {
 function makeFace(type) {
   const face = type === 'O' ? faceYes : faceNo;
 
